4855c5a2d379ce2bac0453918db28a7d46353866
mishka mrelashvili <mishka.mrelashvili@gmail.com>
2026-08-21 21:43:41 +0400

    ruff formatter

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -1,8 +1,13 @@
--- General autocommands.
-
--- Briefly highlight text when you yank (copy) it.
-vim.api.nvim_create_autocmd("TextYankPost", {
+-- Python: on save, run Ruff's import-sort code action, then format.
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.py",
   callback = function()
-    vim.hl.on_yank()
+    -- 1. Organize imports (equivalent to: ruff check --select I --fix)
+    vim.lsp.buf.code_action({
+      context = { only = { "source.organizeImports.ruff" }, diagnostics = {} },
+      apply = true,
+    })
+    -- 2. Format the file (equivalent to: ruff format)
+    vim.lsp.buf.format({ async = false })
   end,
 })
