b442b22defc805e20961b8cf8ddcfb2d9337d255
mishka mrelashvili <mishka.mrelashvili@gmail.com>
2026-09-05 22:11:42 +0400

    clang2.0

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,4 +1,14 @@
 -- C and C++. Binary: sudo dnf install clang-tools-extra
 return {
-  cmd = { "clangd", "--background-index", "--clang-tidy" },
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+  },
+
+  init_options = {
+    fallbackFlags = {
+      "-std=c++20",
+    },
+  },
 }
